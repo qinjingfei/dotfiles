ac3f0f963f47de227575a35b73ce64c45d164da3
Jingfei Qin <qinjingfei1@gmail.com>
2017-10-20 19:13:06 -0700

    add a few software

--- install_with_root.sh
+++ install_with_root.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y \
 	git curl tmux fish screenfetch sl cowsay cmatrix npm python-pip python3-pip  vim\
 	ruby build-essential mpv expect fortune-mod vlc nodejs-legacy htop glances mosh \
-	nmap aria2 tlp powertop silversearcher-ag \
+	nmap aria2 tlp powertop silversearcher-ag software-properties-common \
 	docker docker-compose docker.io     \
 
 sudo npm install -g speed-test gtop browser-sync vue-cli
@@ -15,29 +14,25 @@ sudo pip3 install neovim beautifulsoup4 lxml requests loads mps-youtube youtube_
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 
-sudo add-apt-repository ppa:snwh/pulp
-# update repository info
-sudo apt-get update
-# install icon theme
-sudo apt-get install paper-icon-theme
-# install cursor theme
-sudo apt-get install paper-cursor-theme
-# install gtk theme
-sudo apt-get install paper-gtk-theme
-
 git config --global user.name "Jingfei Qin"
 git config --global user.email "qinjingfei1@gmail.com"
-
 
 #run docker without root
 sudo usermod -aG docker jing
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo docker pull xujinkai/aria2-with-webui
 
 sudo tlp start
 sudo cp ./startup.sh /usr/bin/
 sudo cp ./rc.local /etc/
+sudo cp ./ucsclogin /usr/bin/
 
+# sudo add-apt-repository ppa:snwh/pulp
+# sudo apt-get update
+# sudo apt-get install paper-icon-theme
+# sudo apt-get install paper-cursor-theme
+# sudo apt-get install paper-gtk-theme
 
 clear
 echo "===================="
--- install_without_root.sh
+++ install_without_root.sh
@@ -5,6 +5,13 @@ chsh -s $(which fish)
 wget "https://wallpapersite.com/images/wallpapers/velvet-crowe-3200x1800-hot-anime-girl-hd-6221.jpg"
 wget "http://www.wallpapermaiden.com/image/2017/02/16/hatsune-miku-dark-theme-twintails-vocaloid-anime-13403.jpg"
 wget "http://i.imgur.com/Xyfz5M4.jpg"
-curl -L https://get.oh-my.fish | fish
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher z
+
+wget "https://atom-installer.github.com/v1.21.1/atom-amd64.deb?s=1507768431&ext=.deb"
+wget "http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=KSgsI_LlsOj_HW4mRQ-UXQ&e=1508553597&fn=sogoupinyin_2.1.0.0086_amd64.deb"
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+wget "https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.4.2492.tar.gz"
+
+
+mv *.gz ~/Downloads
+mv *.deb ~/Downloads
+mv *.jpg ~/Picturese
